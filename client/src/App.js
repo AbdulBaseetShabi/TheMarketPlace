7db20931f089a31d8072cdebbb3bf7a1350e6271
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2022-05-13 22:57:35 -0400

    navigatin logic between pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,49 @@
-import './App.css';
-import React from 'react';
+import "./App.css";
+import React from "react";
 
-import Navigation from './widget/Navigation/navigation';
+import Navigation from "./widget/Navigation/navigation";
+
+import Login from "../src/routes/login/login";
+import Signup from "../src/routes/signup/signup";
+import Market from "../src/routes/market/market"
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      current_route: null,
-    }
+      show_nav: false,
+      current_route: "login",
+    };
     this.Navigate = this.Navigate.bind(this);
   }
 
-  Navigate(route){
-    console.log(route);
+  Navigate(route) {
+    let show_nav = route !== "login" && route !== "signup";
+    this.setState({
+      current_route: route,
+      show_nav: show_nav,
+    });
   }
 
   render() {
+    let route = null;
+
+    if (this.state.current_route === "login") {
+      route = <Login Navigate={this.Navigate} />
+    }else if (this.state.current_route === "signup") {
+      route = <Signup Navigate={this.Navigate} />
+    }else if (this.state.current_route === "market") {
+      route = <Market Navigate={this.Navigate} />
+    }
+
     return (
       <div id="main-app">
-        <Navigation Navigate={this.Navigate}/>
+        {this.state.show_nav ? <Navigation Navigate={this.Navigate} /> : null}
         <div id="content">
-
+          {route}
         </div>
       </div>
-    ); 
+    );
   }
 }
 
