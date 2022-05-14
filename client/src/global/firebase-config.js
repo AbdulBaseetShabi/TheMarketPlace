4559944785cc1e6a56477be5a7316998c6edefd1
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2022-05-14 19:27:18 -0400

    firebase module

---
+++ client/src/global/firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAbBUIRFAa6SPRaZPPk3k4TxLb1OhCjXZQ",
+  authDomain: "the-market-place-81075.firebaseapp.com",
+  projectId: "the-market-place-81075",
+  storageBucket: "the-market-place-81075.appspot.com",
+  messagingSenderId: "891440919725",
+  appId: "1:891440919725:web:d5c1f315256a7183f20224",
+  measurementId: "G-EKR8JX1033",
+};
+
+module.exports = {
+  firebaseConfig,
+};
