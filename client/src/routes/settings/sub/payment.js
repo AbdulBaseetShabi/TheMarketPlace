f41aab951a9806af4714a22b8b65ca160784dbc6
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2022-05-15 02:47:14 -0400

    payment template

--- client/src/routes/settings/sub/payment.js
+++ client/src/routes/settings/sub/payment.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../settings.css";
 
 class Payment extends React.Component {
   render() {
@@ -17,150 +18,155 @@ class Payment extends React.Component {
             borderBottom: "1px solid white",
           }}
         />
-        <div id="payment-content">
-        <div id="card">
-          <div>
-            <label className="inline-block header-level-two">
-              Credit Card Information
-            </label>
+        <div id="payment-content" style={{ border: "none" }}>
+          <div id="card">
             <div>
-              <label className="inline-block" style={{ textAlign: "center" }}>
-                Name
+              <label className="inline-block header-level-two">
+                Credit Card Information
               </label>
-              <input
-                type="text"
-                style={{ width: "60%", margin: "0 auto", display: "block" }}
-              />
-            </div>
-            <div>
-              <label className="inline-block" style={{ textAlign: "center" }}>
-                Card No
-              </label>
-              <input
-                type="text"
-                style={{ width: "60%", margin: "0 auto", display: "block" }}
-              />
-            </div>
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <div>
-                <label className="inline-block" style={{ textAlign: "center" }}>
-                  Month
-                </label>
-                <input
-                  type="number"
-                  min="1"
-                  max="12"
+              <div className="section">
+                <div>
+                  <label className="inline-block">Name: </label>
+                  <input
+                    type="text"
+                    style={{ width: "80%", display: "block" }}
+                  />
+                </div>
+                <div>
+                  <label className="inline-block">Card No:</label>
+                  <input
+                    type="text"
+                    style={{ width: "80%", display: "block" }}
+                  />
+                </div>
+                <div
                   style={{
+                    display: "flex",
+                    justifyContent: "start",
                     width: "80%",
-                    margin: "0 auto",
-                    display: "block",
-                    border: "1px solid #cccccc",
-                    height: "30px",
-                    borderRadius: "2px",
-                    boxSizing: "border-box",
-                    outline: 0,
                   }}
-                />
-              </div>
-              <div>
-                <label className="inline-block" style={{ textAlign: "center" }}>
-                  Year
-                </label>
-                <input
-                  type="number"
-                  style={{
-                    width: "50%",
-                    margin: "0 auto",
-                    display: "block",
-                    border: "1px solid #cccccc",
-                    height: "30px",
-                    borderRadius: "2px",
-                    boxSizing: "border-box",
-                    outline: 0,
-                  }}
-                  min="2022"
-                />
-              </div>
-              <div>
-                <label className="inline-block" style={{ textAlign: "center" }}>
-                  CSV
-                </label>
-                <input
-                  type="number"
-                  style={{
-                    width: "30%",
-                    margin: "0 auto",
-                    display: "block",
-                    border: "1px solid #cccccc",
-                    height: "30px",
-                    borderRadius: "2px",
-                    boxSizing: "border-box",
-                    outline: 0,
-                  }}
-                />
+                >
+                  <div>
+                    <label className="inline-block">CSV: </label>
+                    <input
+                      type="number"
+                      style={{
+                        width: "30%",
+                        display: "block",
+                        border: "1px solid #cccccc",
+                        height: "30px",
+                        borderRadius: "2px",
+                        boxSizing: "border-box",
+                        outline: 0,
+                      }}
+                    />
+                  </div>
+                  <div>
+                    <label className="inline-block">Month: </label>
+                    <input
+                      type="number"
+                      min="1"
+                      max="12"
+                      style={{
+                        width: "80%",
+                        display: "block",
+                        border: "1px solid #cccccc",
+                        height: "30px",
+                        borderRadius: "2px",
+                        boxSizing: "border-box",
+                        outline: 0,
+                      }}
+                    />
+                  </div>
+                  <div>
+                    <label className="inline-block">Year: </label>
+                    <input
+                      type="number"
+                      style={{
+                        width: "50%",
+                        display: "block",
+                        border: "1px solid #cccccc",
+                        height: "30px",
+                        borderRadius: "2px",
+                        boxSizing: "border-box",
+                        outline: 0,
+                      }}
+                      min="2022"
+                    />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div id="address">
-          <div>
+          <hr
+            style={{
+              opacity: "0.2",
+              width: "80%",
+              margin: "20px auto",
+              borderBottom: "1px solid white",
+            }}
+          />
+          <div id="address">
             <label className="inline-block header-level-two">
               Billing and Shipping Address
             </label>
-            <div>
-              <label className="inline-block" style={{ textAlign: "center" }}>
-                Address
-              </label>
-              <input
-                type="text"
-                style={{ width: "60%", margin: "0 auto", display: "block" }}
-              />
-            </div>
-            <div style={{ display: "flex", justifyContent: "center" }}>
+            <div className="section">
               <div>
-                <label className="inline-block" style={{ textAlign: "center" }}>
-                  City
-                </label>
-                <input
-                  type="text"
-                  style={{
-                    width: "100%",
-                    margin: "0 auto",
-                    display: "block",
-                  }}
-                />
-              </div>
-              <div>
-                <label className="inline-block" style={{ textAlign: "center" }}>
-                Province
-                </label>
-                <input
-                  type="text"
-                  style={{
-                    width: "30%",
-                    margin: "0 auto",
-                    display: "block",
-                  }}
-                />
-              </div>
-              <div>
-                <label className="inline-block" style={{ textAlign: "center" }}>
-                  Postal Code
-                </label>
-                <input
-                  type="text"
-                  style={{
-                    width: "30%",
-                    margin: "0 auto",
-                    display: "block",
-                  }}
-                />
+                <div>
+                  <label className="inline-block">Address:</label>
+                  <input
+                    type="text"
+                    style={{ width: "80%", display: "block" }}
+                  />
+                </div>
+                <div style={{ display: "flex", justifyContent: "start" }}>
+                  <div>
+                    <label className="inline-block">City:</label>
+                    <input
+                      type="text"
+                      style={{
+                        width: "80%",
+                        display: "block",
+                      }}
+                    />
+                  </div>
+                  <div>
+                    <label className="inline-block">Province:</label>
+                    <input
+                      type="text"
+                      style={{
+                        width: "30%",
+                        display: "block",
+                      }}
+                    />
+                  </div>
+                  <div>
+                    <label className="inline-block">Postal Code:</label>
+                    <input
+                      type="text"
+                      style={{
+                        width: "30%",
+                        display: "block",
+                      }}
+                    />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
+          <hr
+            style={{
+              opacity: "0.2",
+              width: "80%",
+              margin: "20px auto",
+              borderBottom: "1px solid white",
+            }}
+          />
+          <div>
+            <div className="button" style={{backgroundColor: "#329FC4"}}>Edit</div>
+            <div className="button" style={{backgroundColor: "#000000"}}>Cancel</div>
+          </div>
         </div>
-        </div>
-
       </div>
     );
   }
